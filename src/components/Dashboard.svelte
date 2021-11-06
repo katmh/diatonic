81b4d8d2002e274f71e8b53da4e9f5071578290f
katmh <hello@katmh.com>
2021-11-06 00:59:11 -0400

    refactor dashboard into component

---
+++ src/components/Dashboard.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import { rulers, rulerInfo } from "../stores.js";
+    import presets from "../data/presets.js";
+
+    export let handleSelectMode: () => void;
+    export let selectedMode: string;
+</script>
+
+<div class="dashboard">
+    <label for="mode">select a mode:</label>
+    <select
+        name="mode"
+        id="mode"
+        bind:value={selectedMode}
+        on:change={handleSelectMode}
+    >
+        <option value="" />
+        {#each presets as preset}
+            <option value={preset}>{preset.label}</option>
+        {/each}
+    </select>
+
+    <br />
+    <br />
+
+    {#each $rulers as ruler}
+        <p>{$rulerInfo[ruler.type].inWindow}</p>
+    {/each}
+</div>
+
+<style>
+    .dashboard {
+        width: 200px;
+        padding-top: 1rem;
+        font: 1rem/1.25 system-ui;
+    }
+</style>
